354a6dde8dd7ae002d7436415edc395f5257fdab
Steven Deobald <steven@deobald.ca>
2011-02-14 11:27:00 -0600

    plugin added

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject lein-fig/lein-fig "1.0.0"
+  :description "Clobber files in your build."
+  
+  :eval-in-leiningen true
+  :min-lein-version "1.4.0"
+
+  :dev-dependencies [[lein-clojars "0.6.0"]])
---
+++ src/leiningen/clobber.clj
@@ -0,0 +1,27 @@
+(ns leiningen.clobber "clobber"
+    (:use [leiningen.clean]
+          [lancet :only [delete fileset]]
+          [clojure.contrib.io :only [file]]))
+
+(defn dir? [f]
+  (.isDirectory (file f)))
+
+(defn dirs-only [clobberables]
+  (filter dir? clobberables))
+
+(defn files-only [clobberables]
+  (filter (complement dir?) clobberables))
+
+(defn- clobber-dirs [files]
+  (doseq [to-delete (dirs-only files)]
+    (delete {:dir to-delete})))
+
+(defn- clobber-files [files]
+  (let [to-delete (apply str (interpose "," (files-only files)))]
+    (delete {} (fileset {:dir "." :includes to-delete}))))
+
+(defn clobber [project & args]
+  (let [files (:clobber project)]
+    (clobber-files files)
+    (clobber-dirs files)
+    (clean project)))
