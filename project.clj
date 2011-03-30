a6100a4aa20ca696b9ae789c54f7fa065a742636
Steven Deobald <steven@deobald.ca>
2011-03-30 10:52:55 -0500

    lancet is now an extra dep; bumping to 1.0.2

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject lein-clobber/lein-clobber "1.0.1"
+(defproject lein-clobber/lein-clobber "1.0.2"
   :description "Delete files and directories"
   
   :eval-in-leiningen true
   :min-lein-version "1.5.0"
 
+  :dependencies [[lancet "1.0.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
